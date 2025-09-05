cd5be540cf0752bda74ca2c6367210ef18fd2555
katrinlif95 <katrinlif95@gmail.com>
2025-09-05 09:56:58 +0000

    hallo

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 Member 1: Katrín Líf Þórðardóttir
 Member 2: Esther Helena Hauksdóttir
+Member 3: Kimberley Dóra Hjálmarsdóttir
--- esther_1.py
+++ esther_1.py
@@ -4,3 +4,5 @@ print(number)
 for i in range(1, number):
     number = number-1
     print(number)
+    print('Hahahaha')
+    
